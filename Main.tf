b95c2b4b3866f875260700dc5549e1985f8dc2cf
mjulestek <mjules.tek@gmail.com>
2026-02-04 16:12:26 +0100

    Add files via upload

--- Main.tf
+++ Main.tf
@@ -3,3 +3,24 @@ provider "aws" {
     access_key = "ASIAQPXWIOJCZDZDKCJW"
     secret_ket = "cj3IFyYwdruCbIAz9lRg+hpKwSOdQpxEBzYkb/3d"
 }
+
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true 
+  enable_dns_support = true
+
+  tags       =  {
+    name     = "deham9"
+  }
+}
+
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham9"
+  }
+}
