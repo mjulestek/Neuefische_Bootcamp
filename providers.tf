f0d14fe8afefa31a0470bd9800d5c8713240d996
mjulestek <mjules.tek@gmail.com>
2026-02-04 16:41:05 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.30.0"
+        source = "hashicorp/aws"
+        version = "~> 3.0"
     }
   }
+}
+
 provider "aws" {
   region = "us-west-2"
-}
 }
