42a43918ca76021e4ec498c6d20ce41d89f718ca
mjulestek <mjules.tek@gmail.com>
2026-02-04 16:41:28 +0100

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,14 +1,12 @@
-
 resource "aws_vpc" "dev_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true 
   enable_dns_support = true
 
   tags       =  {
-    name     = "deham9"
+    name     = "deham33"
   }
 }
-
 resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.dev_vpc.id
   cidr_block = "10.0.1.0/24"
@@ -16,6 +14,6 @@ resource "aws_subnet" "public-1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "deham9"
+    Name = "deham33"
   }
 }
